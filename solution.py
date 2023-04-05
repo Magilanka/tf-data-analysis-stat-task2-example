6320adc9be6ecb32e19e7916aba890757e2bdfc7
Марина <Magilanka@users.noreply.github.com>
2023-04-05 13:16:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ from scipy.stats import norm
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    from statsmodels.distributions.empirical_distribution import ECDF
     alpha = 1 - p
     a = 0.056
     ecdf = ECDF(x) # ECDF показывают каждую точку данных, и график можно интерпретировать только одним способом. В статистике эмпирическая функция распределения (обычно также называемая эмпирической кумулятивной функцией распределения, ecdf) представляет собой функцию распределения, связанную с эмпирическим показателем выборки. Эта кумулятивная функция распределения представляет собой ступенчатую функцию, которая увеличивается на 1/n в каждой из n точек данных. https://notebook.community/maxis42/ML-DA-Coursera-Yandex-MIPT/1%20Mathematics%20and%20Python/Lectures%20notebooks/12%20sample%20distribution%20evaluation/sample_distribution_evaluation
