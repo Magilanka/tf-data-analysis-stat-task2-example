cbcc69363d2f22f3cf3434194ab2df9f297012f6
Марина <Magilanka@users.noreply.github.com>
2023-04-07 14:53:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ from scipy.stats import norm
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    from scipy.stats import uniform
     n = len(x)
     q = uniform.ppf(p, loc=0.056, scale=1-0.056)
     mean = np.mean(x)
